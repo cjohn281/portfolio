423a518a942f5fc5155b5f3fbb11e12217292f4a
clickspark <49884892+clickspark@users.noreply.github.com>
2020-09-14 01:59:41 -0400

    Updates

---
+++ precache-manifest.7b73e3072abb56f85ebdebdfa4527fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17535c3c4812123033a067f36cfe0e2e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3514d3faf2c229b8d376",
+    "url": "/portfolio/static/css/main.bf33d425.chunk.css"
+  },
+  {
+    "revision": "e4dab5a8b294d908fb0c",
+    "url": "/portfolio/static/js/2.b24a7e70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b24a7e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3514d3faf2c229b8d376",
+    "url": "/portfolio/static/js/main.a830bcd7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "3ca51f004bba63d9cf4018a82d103f2c",
+    "url": "/portfolio/static/media/resume.3ca51f00.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f0ce99799c8abd96e992b148d9fd00f.js"
+  "/portfolio/precache-manifest.7b73e3072abb56f85ebdebdfa4527fa3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
