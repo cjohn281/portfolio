831e866a87affc3aafea3f004534a9eeca246a43
clickspark <49884892+clickspark@users.noreply.github.com>
2020-09-21 18:54:26 -0400

    Responsive styling added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,9 @@ import './Header.css';
 
 const header = props => (
 
-	<div className="header" />
-
+	<div className="header">
+		<div className="main-heading">Christopher Johnson</div>
+	</div>
 );
 
 export default header;
